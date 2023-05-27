bc0267136337373085c981b08373be39d8819a65
janne kalliokulju <janne.kalliokulju@gmail.com>
2023-05-27 17:16:03 +0300

    12.13

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -5,5 +5,5 @@ const REDIS_URL='redis://localhost:6378'
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'a
+  REDIS_URL//: '//localhost:6378'
 }
